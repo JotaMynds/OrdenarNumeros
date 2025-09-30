32ba87c32c1542738e9bc80344fb6230681e6f61
JotaMynds <ivansanjuar@gmail.com>
2025-09-30 08:25:49 +0200

    V1.0

--- src/main/java/ordenarNumeros/ordenarNumeros.java
+++ src/main/java/ordenarNumeros/ordenarNumeros.java
@@ -3,6 +3,7 @@ package ordenarNumeros;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 
 public class ordenarNumeros {
     public static void main(String[] args) {
@@ -10,37 +11,38 @@ public class ordenarNumeros {
         //Creamos el isr y el br para interactuar con los archivos
         InputStreamReader isr = new InputStreamReader(System.in);
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String Linea= null;
+        String Linea = null;
         //Estructura try catch para la ejecución
-        try{
+        try {
+            int[] numeros;
             //Bucle para convertir los numeros
-            while((Linea = br.readLine()) != null){
+            while ((Linea = br.readLine()) != null) {
 
                 //Crea el array para guardar los numeros individualmente
-                String [] Dato = Linea.split(" ");
+                String[] Dato = Linea.split(" ");
 
                 //Creamos el array con la longitud del array con los numeros
-                int [] numeros = new int [Dato.length];
+                 numeros = new int[Dato.length];
 
                 //Convertimos a numeros los String guardados como números
-                for(int i=0; i<Dato.length; i++){
+                for (int i = 0; i < Dato.length; i++) {
                     numeros[i] = Integer.parseInt(Dato[i]);
                 }
-
+                Arrays.sort(numeros);
             }
-
-        }catch (IOException e){
+        } catch (IOException e) {
             System.out.println(e.getMessage());
-        }finally{
-            try{
-
-            }catch (IOException e2){
+        } finally {
+            try {
+                br.close();
+                isr.close();
+            } catch (IOException e2) {
                 System.out.println("Error en ordenarNumeros.");
             }
         }
 
 
-        }
-
-
     }
+
+
+}
